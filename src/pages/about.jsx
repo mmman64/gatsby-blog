76c432551ca991f185fe45e7e7c0b7eb111175a0
Marco-FI-SEI <mmc90@hotmail.co.uk>
2020-04-11 15:26:38 +0100

    📦 NEW: Add: Init styles

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,16 @@
-import React, { Fragment } from "react";
-import  { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from "../components/layout";
 
 export default () => (
-  <Fragment>
+  <Layout>
     <h1>About Me</h1>
-    <p>I'm Marco, an Irish Italian <strike>Stallion</strike> developer from London!</p>
+    <p>
+      I'm Marco, an Irish Italian <strike>Stallion</strike> developer from
+      London!
+    </p>
     <p>Into Ruby, JavaScript, Cloud, high-end headphones.</p>
 
     <Link to="/">&larr; Home</Link>
-  </Fragment>
-)
+  </Layout>
+);
