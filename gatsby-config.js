6ea764e2b35ec05e4c7bcedac64cef01c5f94634
mmman64 <mcnultymarco@gmail.com>
2020-04-11 16:47:31 +0100

    📦 NEW: Add: site metadata hook, mdx integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Marco McNulty',
+    description:
+      'A place to write something interesting to me...and maybe to you.',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.jsx'),
+        },
+      },
+    },
+  ],
 };
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,68 +1,80 @@
 import React, { Fragment } from 'react';
 import { Global, css } from '@emotion/core';
+import { Helmet } from 'react-helmet';
 import Header from './header';
+import useSiteMetaData from '../hooks/use-sitemetadata';
 
-const Layout = ({ children }) => (
-  <Fragment>
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-        }
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetaData();
 
-        * + * {
-          margin-top: 1rem;
-        }
-
-        html,
-        body {
-          margin: 0;
-          color: red;
-          font-family: 'Roboto', sans-serif;
-          font-size: 18px;
-          line-height: 1.4;
-
-          /* remove margin for the div Gatsby mounts into */
-          > div {
-            margin-top: 0;
+  return (
+    <Fragment>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
           }
-        }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          color: #222;
-          line-height: 1.1;
-
-          + * {
-            margin-top: 0.5rem;
+          * + * {
+            margin-top: 1rem;
           }
-        }
 
-        strong {
-          color: #222;
-        }
+          html,
+          body {
+            margin: 0;
+            color: red;
+            font-family: 'Roboto', sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
 
-        li {
-          margin-top: 0.25rem;
-        }
-      `}
-    />
-    <Header />
-    <main
-      css={css`
-        margin: 2rem auto 4rem;
-        max-width: 90vw;
-        width: 550px;
-      `}
-    >
-      {children}
-    </main>
-  </Fragment>
-);
+            /* remove margin for the div Gatsby mounts into */
+            > div {
+              margin-top: 0;
+            }
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+
+            + * {
+              margin-top: 0.5rem;
+            }
+          }
+
+          strong {
+            color: #222;
+          }
+
+          li {
+            margin-top: 0.25rem;
+          }
+        `}
+      />
+      {/* Helmet lets you modify the head of a React document */}
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 550px;
+        `}
+      >
+        {children}
+      </main>
+    </Fragment>
+  );
+};
 
 export default Layout;
